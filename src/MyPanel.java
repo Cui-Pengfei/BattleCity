04203e467ca225b53ac1085b55e9c361e0cb5ba3
cpf <cpfprogrammer@163.com>
2021-07-01 15:41:33 +0800

    上下左右静态坦克都绘制好了

--- src/MyPanel.java
+++ src/MyPanel.java
@@ -14,9 +14,11 @@ public class MyPanel extends JPanel{//我的画板
 		super.paint(g);
 		g.setColor(Color.DARK_GRAY);//设置画板的背景颜色
 		g.fillRect(0, 0, 800, 800);
-		drawTank(g, 200, 200, Tank.UP, Tank.HERO);
-		drawTank(g, 300, 200, Tank.UP, Tank.ENEMY);
-		drawTank(g, 400, 200, Tank.UP, Tank.BOSS);
+
+		drawTank(g, 100, 200, Tank.UP, Tank.HERO);
+		drawTank(g, 200, 200, Tank.DOWN, Tank.ENEMY);
+		drawTank(g, 300, 200, Tank.LEFT, Tank.HERO);
+		drawTank(g, 400, 200, Tank.RIGHT, Tank.BOSS);
 	}
 
 	public void drawTank(Graphics g, int x, int y, int direction, int type){
@@ -38,7 +40,7 @@ public class MyPanel extends JPanel{//我的画板
 		}
 
 		switch(direction){
-			case Tank.UP:
+			case Tank.UP://炮口朝上的坦克
 				g.fill3DRect(x, y, 10, 60, false);//轮子
 				g.fill3DRect(x + 30, y, 10, 60, false);//轮子
 				g.fill3DRect(x + 10, y + 10, 20, 40, false);//车体
@@ -49,6 +51,46 @@ public class MyPanel extends JPanel{//我的画板
 				g.setColor(Color.orange);
 				g.drawLine(x + 20, y + 30, x + 20, y);
 				break;
+
+			case Tank.DOWN://炮口朝下的坦克
+				g.fill3DRect(x, y, 10, 60, false);//轮子
+				g.fill3DRect(x + 30, y, 10, 60, false);//轮子
+				g.fill3DRect(x + 10, y + 10, 20, 40, false);//车体
+
+				g.setColor(Color.green);
+				g.fillOval(x + 10, y + 20, 20, 20);//炮台
+
+				g.setColor(Color.orange);
+				g.drawLine(x + 20, y + 30, x + 20, y + 60);//炮管
+				break;
+
+			case Tank.LEFT://炮口朝左的坦克
+				g.fill3DRect(x - 10, y + 10, 60, 10, false);//车轮
+				g.fill3DRect(x - 10, y + 40, 60, 10, false);//车轮
+				g.fill3DRect(x, y + 20, 40, 20, false);//车体
+
+
+				g.setColor(Color.green);
+				g.fillOval(x + 10, y + 20, 20, 20);//炮台
+
+				g.setColor(Color.orange);
+				g.drawLine(x + 20, y + 30, x - 10, y + 30);//炮管
+				break;
+
+			case Tank.RIGHT:
+				g.fill3DRect(x - 10, y + 10, 60, 10, false);//车轮
+				g.fill3DRect(x - 10, y + 40, 60, 10, false);//车轮
+				g.fill3DRect(x, y + 20, 40, 20, false);//车体
+
+
+				g.setColor(Color.green);
+				g.fillOval(x + 10, y + 20, 20, 20);//炮台
+
+				g.setColor(Color.orange);
+				g.drawLine(x + 20, y + 30, x + 50, y + 30);//炮管
+				break;
+
+
 			default:
 				System.out.println("其他方向暂时没有绘制...");
 
